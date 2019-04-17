c80d7589630268da232d152c5e299d2e5ed064a0
John <44563025+JLam63@users.noreply.github.com>
2019-04-17 10:35:31 +1000

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { FormsModule, ReactiveFormsModule  }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
+//import { HttpModule } from '@angular/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule ,
+    HttpClientModule,
+    //HttpModule      
   ],
+  exports:[FormsModule,ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
